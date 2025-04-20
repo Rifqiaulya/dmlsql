decb9a32f89bd9aa763eb1fc79237433c842926d
Rifqiaulya <72006057+Rifqiaulya@users.noreply.github.com>
2025-04-20 20:13:49 +0800

    Update scriptdml.sql

--- scriptdml.sql
+++ scriptdml.sql
@@ -1,4 +1,8 @@
--- DML Data Manipulation Language (DML) adalah bahasa untuk mengkueri informasi dari database dengan akses seperti memasukkan, menghapus, dan memodifikasi baris dalam database
+-- DML Data Manipulation Language (DML) adalah bahasa untuk manipulasi database
+-- MEMBUAT DATABASE
+CREATE DATABASE testDB;
+USE testDB;
+-- MEMBUAT TABEL
 CREATE TABLE products
 (
     id          VARCHAR(10)  NOT NULL,
@@ -8,3 +12,19 @@ CREATE TABLE products
     quantity    INT UNSIGNED NOT NULL DEFAULT 0,
     created_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+-- MELIHAT TABEL DAN MENDESKRIPSIKANNYA
+SHOW TABLES;
+DESCRIBE products;
+-- ALIAS
+-- ALIAS UNTUK KOLOM (ingin mengubah nama kolom atau nama tabel ketika melakukan SELECT )
+SELECT id       AS Kode,
+       name     AS Nama,
+       category AS Kategori,
+       price    AS Harga,
+       quantity AS Jumlah
+FROM products;
+
+
+
+
+
